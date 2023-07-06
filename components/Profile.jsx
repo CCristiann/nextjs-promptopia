8cdc3b321e552676edaf19af9154fd64d9807ec4
Cristian Cirje <127857266+CCristiann@users.noreply.github.com>
2023-07-06 19:13:48 +0200

    Fixed Error occurred prerendering page "/profile"

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { use, useEffect } from "react";
 import PromptCard from "./PromptCard";
 import { ToastContainer } from "react-toastify";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Profile = ({
   name,
@@ -21,45 +21,45 @@ const Profile = ({
       name = "My"
       desc= "Welcome to your profile page"
     }
-  
+
     if(session?.user.id || userId){
       return(
         <>
         <section className="w-full">  
-        <div className="flex flex-col gap-4">
-          <h1 className="profile_text">{name} Profile</h1>
-          <p className="description">{desc}</p>
+      <div className="flex flex-col gap-4">
+        <h1 className="profile_text">{name} Profile</h1>
+        <p className="description">{desc}</p>
+      </div>
+
+      {posts.length == 0 ? (
+        <div className="w-full py-16">
+          <h5 className="mt-5 text-gray-900 font-medium text-2xl md:text-3xl max-w-2xl">
+            Create a post to see your personal posts in this section!
+          </h5>
         </div>
-  
-        {posts.length == 0 ? (
-          <div className="w-full py-16">
-            <h5 className="mt-5 text-gray-900 font-medium text-2xl md:text-3xl max-w-2xl">
-              Create a post to see your personal posts in this section!
-            </h5>
-          </div>
-        ) : (
-          <div className="prompt_layout">
-            {posts.map((post) => (
-              <PromptCard
-                key={post._id}
-                post={post}
-                handleTagClick={handleTagClick}
-                handleDelete={() => handleDelete && handleDelete(post)}
-                handleEdit={() => handleEdit && handleEdit(post)}
-              />
-            ))}
-          </div>
-        )}
-  
-        <ToastContainer />
-  
+      ) : (
+        <div className="prompt_layout">
+          {posts.map((post) => (
+            <PromptCard
+              key={post._id}
+              post={post}
+              handleTagClick={handleTagClick}
+              handleDelete={() => handleDelete && handleDelete(post)}
+              handleEdit={() => handleEdit && handleEdit(post)}
+            />
+          ))}
+        </div>
+      )}
+
+      <ToastContainer />
+
       </section>
         </>
-      )
+     )
     }else{
       router.push('/')
     }
-  }, [])
+  }, [router])
   
 };
 
